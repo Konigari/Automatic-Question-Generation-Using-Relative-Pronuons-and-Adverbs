d7f32a7b032089db3aa58d26e3be821f7c7dae44
mukulhase <mukulhase@gmail.com>
2018-02-02 03:28:35 +0530

    Generate 1st questions

--- rcqg.py
+++ rcqg.py
@@ -1,4 +1,5 @@
 import spacy
+import inspect
 nlp = spacy.load('en')
 
 def filetotext():
@@ -16,10 +17,32 @@ def show(u_line):
 		print(ent.text, ent.start_char, ent.end_char, ent.label_) 
 	print("SENTENCE DONE")
 
+def props(obj):
+    pr = {}
+    for name in dir(obj):
+        value = getattr(obj, name)
+        if not name.startswith('__') and not inspect.ismethod(value):
+            pr[name] = value
+    return pr
+
 def genq(sentence):
-	doc = nlp(u_line)
+	def getAtt(obj):
+		return {
+			'tag_': obj.tag_,
+			'dep_': obj.dep_
+		}
+
+	def filt(d2):
+		return lambda x:set(getAtt(x).items()).issubset(set(d2.items()))
+
+	doc = nlp(sentence)
 	att = {
-		tag_ : 'WP',
-		dep_ : 'nsubj'
+		'tag_' : 'WP',
+		'dep_' : 'nsubj'
 	}
-	filter(lambda x:token.)
+	words = (list(filter(lambda tup: filt(att)(tup[1]), enumerate(doc))))
+	word = words[0]
+	index = word[0]
+	print(word[1].text + " " + " ".join(sentence.split(" ")[index+1:])+"?")
+
+	## who did they give the chocolate to

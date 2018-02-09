051f5cf3edf927051748413399ebf87a44c25dae
mukulhase <mukulhase@gmail.com>
2018-02-09 18:49:51 +0530

    Incomplete

--- rcqg.py
+++ rcqg.py
@@ -1,5 +1,6 @@
 import spacy
 import inspect
+from spacy import displacy
 nlp = spacy.load('en')
 
 
@@ -18,6 +19,10 @@ def show(u_line):
 		print(ent.text, ent.start_char, ent.end_char, ent.label_) 
 	print("SENTENCE DONE")
 
+def display(u_line):
+	doc = nlp(u_line)	
+	displacy.serve(doc, style='dep')
+
 def props(obj):
     pr = {}
     for name in dir(obj):
@@ -27,23 +32,39 @@ def props(obj):
     return pr
 
 def genq(sentence):
-	def getAtt(obj):
+	def serialize(obj):
 		return {
 			'tag_': obj.tag_,
-			'dep_': obj.dep_
+			'dep_': obj.dep_,
+			'pos_': obj.pos_,
+
 		}
 
 	def filt(d2):
-		return lambda x:set(getAtt(x).items()).issubset(set(d2.items()))
+		return lambda x:set(d2.items()).issubset(set(serialize(x).items()))
+
+	def filteratt(att, doc):
+		return (list(filter(lambda tup: filt(att)(tup[1]), enumerate(doc))))
 
 	doc = nlp(sentence)
+
 	att = {
 		'tag_' : 'WP',
 		'dep_' : 'nsubj'
 	}
-	words = (list(filter(lambda tup: filt(att)(tup[1]), enumerate(doc))))
-	word = words[0]
-	index = word[0]
-	print(word[1].text + " " + " ".join(sentence.split(" ")[index+1:])+"?")
+	relativeclauses = filteratt(att, doc)
+	for word in relativeclauses:
+		index = word[0]
+		relclause = word[1]
+		print(word[1].text + " " + " ".join(sentence.split(" ")[index+1:])+"?")
+		verb_past = filteratt({
+			'tag_':'VBD',
+			'dep_':'ROOT'
+			}, doc)
+		for verb in verb_past:
+			if relclause.text.lower()=='who':
+				print("Who "+"did "+ next(doc.noun_chunks).text + " " + verb[1].lemma_ + " " + "to meet")
 
-	## who did they give the chocolate to
+	# relativeclause = 
+
+	## who did they give the chocolate t
